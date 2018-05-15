c94d8a6c315404176f09aed8a7f07b82459b2333
何广忠 <heguangzhong@nosppp.com>
2018-05-15 17:46:55 +0800

    update
    update

---
+++ VORouter/VORouter/VORouterManager.h
@@ -0,0 +1,17 @@
+//
+//  VORouterManager.h
+//  VORouter
+//
+//  Created by 何广忠 on 2018/5/15.
+//  Copyright © 2018年 heguangzhong2009@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VORouterManager : NSObject
+
+/**
+ *  跳转界面
+ */
+- (void)push:(NSDictionary *)params;
+@end
